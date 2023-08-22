8c912dc72562bbc1494e81d0b1a27225b8539f88
programmer-weeb <forprogramming8@gmail.com>
2023-08-22 20:36:59 +0300

    Switch to pug

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const catalogRouter = require('./routes/catalog')
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(logger('dev'));
 app.use(express.json());
